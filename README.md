3d327e66dfca01afd1eee7a90042a6dd3cc7e326
jarbasal <jarbasai@mailfence.com>
2020-02-26 06:02:07 +0000

    hivemind webchat - AES support

--- README.md
+++ README.md
@@ -19,11 +19,9 @@ Access from web browser http://your_ip_address:9090
 
 Securing tornado is out of scope for this repo, it is currently served by HTTP, you probably want to set up nginx or equivalent with [let's encrypt](https://letsencrypt.org/) certificates
 
-Hivemind connection is http by default! This is because browsers reject self signed certs, you probably want to use [let's  encrypt](https://letsencrypt.org/) certificates and configure [HiveMind-core](https://github.com/OpenJarbas/HiveMind-core) to use them
+Hivemind connection is http by default! This is because browsers reject self signed certs, you may want to use [let's  encrypt](https://letsencrypt.org/) certificates and configure [HiveMind-core](https://github.com/OpenJarbas/HiveMind-core) to use them
 
-Hivemind Encryption is not supported yet (don't set crypto key in [HiveMind-core](https://github.com/OpenJarbas/HiveMind-core))
-
-Basically this is unsafe to run outside your home network with default configurations
+Hivemind Encryption is supported
 
 ## Credits
 
--- tornado_webchat/static/js/app.js
+++ tornado_webchat/static/js/app.js
@@ -8,22 +8,117 @@
 -------------------------------------------------------------
 
 */
+// HiveMind socket
+const user = "Webchat_user";
+const key = "webchat_key";
+const ip = "0.0.0.0";
+const port = 5678;
+const hivemind_address = 'ws://' + ip + ":" + port;
+// HIGHLY recommended to set this if using ws instead of wss
+// check examples/add_keys.py under hivemind-core
+const crypto_key = "ivf1NQSkQNogWYyr";
+
+/*
+Import an AES secret key from an ArrayBuffer containing the raw bytes.
+Takes an ArrayBuffer string containing the bytes, and returns a Promise
+that will resolve to a CryptoKey representing the secret key.
+*/
+function importSecretKey(rawKey) {
+    return crypto.subtle.importKey(
+        "raw",
+        rawKey,
+        "AES-GCM",
+        false,
+        ["encrypt", "decrypt"]
+    );
+}
+
+function toHexString(byteArray) {
+    return Array.from(byteArray, function (byte) {
+        return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join('')
+}
+
+function bufferToHex(buffer) {
+    return Array
+        .from(new Uint8Array(buffer))
+        .map(b => b.toString(16).padStart(2, "0"))
+        .join("");
+}
+
+
+function fromHexString(hexString) {
+    return new Uint8Array(hexString.match(/.{1,2}/g).map(byte => parseInt(byte, 16)))
+}
+
+async function decrypt_msg(hex_ciphertext, hex_iv) {
+    // IV must be the same length (in bits) as the key
+    let iv = fromHexString(hex_iv);
+
+    let encryption_key = await importSecretKey(new TextEncoder().encode(crypto_key));
+
+    let decrypted = await crypto.subtle.decrypt({
+        name: 'AES-GCM',
+        iv
+    }, encryption_key, fromHexString(hex_ciphertext))
+    return String.fromCharCode.apply(null, new Uint8Array(decrypted));
+
+}
+
+async function encrypt_msg(text) {
+    // IV must be the same length (in bits) as the key
+    let iv = await crypto.getRandomValues(new Uint8Array(16))
+
+    let encryption_key = await importSecretKey(new TextEncoder().encode(crypto_key));
+
+    let cyphertext = await crypto.subtle.encrypt({
+        name: 'AES-GCM',
+        iv
+    }, encryption_key, new TextEncoder().encode(text))
+    return {"nonce": toHexString(iv), "ciphertext": bufferToHex(cyphertext)}
+
+}
 
 
 $(document).ready(function () {
 
-    // HiveMind socket
-    var user = "dummy_user";
-    var key = "dummy_key";
-    let ip = "0.0.0.0";
-    let port = 5678;
     var authToken = btoa(user + ":" + key);
 
     document.cookie = 'X-Authorization=' + authToken + '; path=/';
 
-    var ws = new WebSocket('ws://' + ip + ":" + port);
+    var ws = new WebSocket(hivemind_address);
 
-    console.log('ws://' + ip + ":" + port);
+    console.log(hivemind_address);
+
+
+    async function handle_bus_message(message) {
+        if (crypto_key && message.ciphertext) {
+            if (message.tag) {
+                message = await decrypt_msg(message["ciphertext"] + message["tag"], message["nonce"])
+            } else {
+                message = await decrypt_msg(message["ciphertext"], message["nonce"])
+            }
+            message = JSON.parse(message)
+        }
+
+
+        if (message.msg_type === "bus") {
+            let mycroft_message = JSON.parse(message.payload);
+            if (mycroft_message.type === "speak") {
+                let utterance = mycroft_message.data.utterance;
+                push_response(utterance)
+            }
+        }
+    }
+
+    async function sendMessage(message) {
+
+        let hive_msg = message;
+        if (crypto_key) {
+            message = await encrypt_msg(JSON.stringify(hive_msg))
+        }
+        ws.send(JSON.stringify(message));
+    }
 
 
     ws.onopen = function () {
@@ -33,28 +128,12 @@ $(document).ready(function () {
 
     ws.onmessage = function (message) {
         message = JSON.parse(message.data);
-
-        if (message.msg_type === "bus"){
-            let mycroft_message =JSON.parse(message.payload);
-            console.log("Received: " + mycroft_message.type);
-            if (mycroft_message.type === "speak"){
-                let utterance = mycroft_message.data.utterance;
-                push_response(utterance)
-            }
-        }
+        handle_bus_message(message)
 
     };
 
     ws.onclose = function () {
         console.log("disconnected");
-    };
-
-    var sendMessage = function (message) {
-        // TODO hivemind message
-        let hive_msg = message
-
-        console.log("sending:" + JSON.stringify(hive_msg));
-        ws.send(JSON.stringify(message));
     };
 
 
@@ -87,13 +166,16 @@ $(document).ready(function () {
     function get_resp(q) {
         let payload = {
             'type': "recognizer_loop:utterance",
-            "data":{"utterances": [q]},
-            "context": {"source": "WebChat", "destination": "HiveMind", "platform": "JarbasWebchatTerminalV0.1"}
+            "data": {"utterances": [q]},
+            "context": {
+                "source": "WebChat",
+                "destination": "HiveMind",
+                "platform": "JarbasWebchatTerminalV0.1"
+            }
         };
         sendMessage({
             'msg_type': "bus",
-            "payload": payload,
-            "context": {"source": "WebChat", "destination": "HiveMind", "platform": "JarbasWebchatTerminalV0.1"}
+            "payload": payload
         });
 
     }
